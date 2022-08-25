e628935efeae5f46d0480c7b9520f85bdef406f8
nemo0 <subhachanda88@gmai.com>
2022-08-25 19:32:03 +0530

    added counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  // Write a useEffect function to increment and decrement
+  useEffect(() => {
+    console.log('useEffect');
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 1000);
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <p>{count}</p>
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
+      <div></div>
     </div>
   );
 }
